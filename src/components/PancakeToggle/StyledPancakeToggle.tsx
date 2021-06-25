ac70ed37788e4a12df3fabe1528cb10cf653842a
Literally Collective <bluedragon.cstar@outlook.com>
2021-06-25 06:24:03 -0300

    Feat: update contract color

--- src/components/PancakeToggle/StyledPancakeToggle.tsx
+++ src/components/PancakeToggle/StyledPancakeToggle.tsx
@@ -140,7 +140,7 @@ export const PancakeInput = styled.input<InputProps>`
   }
 
   &:checked + label .pancake:nth-child(1) {
-    background: #c639e0;
+    background: #061719;
     box-shadow: 0 ${getScale("pancakeThickness")} 0 ${getScale("pancakeThickness")} #fbbe7c;
     transition-delay: 0.2s;
   }
